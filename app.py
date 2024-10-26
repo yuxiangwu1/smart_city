783a3f2eabee35c929abc02102e743d698cedf86
yuwu-server-side <yuwu8011@uni.sydney.edu.au>
2024-10-26 22:48:34 +0000

    fix: workflow

--- app.py
+++ app.py
@@ -450,4 +450,4 @@ def adjust_itinerary():
 
 # Start application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+PID=$(ps aux | grep '[a]pp.py' | awk '{print $2}')
+
+if [ -z "$PID" ]; then
+  echo "smart_city is currently NOT running"
+else
+  echo "stopping smart_city (PID: $PID)..."
+  kill -9 $PID
+  echo "smart_city stopped。"
+fi
 ./smart_city/bin/pip3  install -r requirements.txt 
 
-./smart_city/bin/python3 app.py
+nohup ./smart_city/bin/python3 app.py --host=0.0.0.0 --port=5000 > app.log 2>&1 &
